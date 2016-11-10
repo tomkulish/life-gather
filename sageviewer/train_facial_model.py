10ba7f5794afaf029ba88ba088e73d8ac3b4a769
Thomas Kulish <tkulish@clearedgeit.com>
2016-11-10 14:41:06 -0500

    Added in the training and testing of images dataset.

--- sageviewer/train_facial_model.py
+++ sageviewer/train_facial_model.py
@@ -4,11 +4,12 @@ import os
 import urllib
 import json
 import cv2
+from PIL import Image
 
 # define the path to the face detector
 FACE_DETECTOR_PATH = "/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(FACE_DETECTOR_PATH);
-#recognizer = cv2.createLBPHFaceRecognizer()
+recognizer = cv2.face.createLBPHFaceRecognizer()
 
 def grab_image(path=None, stream=None, url=None):
     # if the path is not None, then load the image from disk
@@ -44,8 +45,8 @@ def find_faces(image):
     data ={"num_faces": len(rects), "faces": rects, "success": True}
     print data
 
-def get_images_and_labels(path):
-    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.sad')]
+def get_images_and_labels(path, nbr):
+    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.test')]
 
     # images will contains face images
     images = []
@@ -56,7 +57,7 @@ def get_images_and_labels(path):
         image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
         # Get the label of the image
         #nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
-        nbr = os.path.split(image_path)[1].split(".")[0]
+        #nbr = os.path.split(image_path)[1].split(".")[0]
         # Detect the face in the image
         faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
         # If face is detected, append the face to images and the label to labels
@@ -68,7 +69,38 @@ def get_images_and_labels(path):
     # return the images list and labels list
     return images, labels
 
-images, labels = get_images_and_labels('/home/tkulish/life-gather/sageviewer/train-images')
+#images, labels = get_images_and_labels('/home/tkulish/life-gather/sageviewer/train-images')
+images, labels = get_images_and_labels('/home/tkulish/life-gather/sageviewer/train-olivia', 1)
+images2, labels2 = get_images_and_labels('/home/tkulish/life-gather/sageviewer/train-tom', 2)
+images.extend(images2)
+labels.extend(labels2)
 print labels
 
 # Train the model using a dataset in 
+recognizer.train(images, np.array(labels))
+
+# Append the images with the extension .sad into image_paths
+path = '/home/tkulish/life-gather/sageviewer/train-images'
+image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.test')]
+for image_path in image_paths:
+    print("path:" + image_path)
+    image = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+    predict_image_pil = Image.open(image_path).convert('L')
+    predict_image = np.array(predict_image_pil, 'uint8')
+    faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in faces:
+        result = cv2.face.MinDistancePredictCollector()
+        recognizer.predict(predict_image[y: y + h, x: x + w],result, 0)
+        nbr_predicted = result.getLabel()
+        conf = result.getDist()
+        #nbr_predicted = recognizer.predict(predict_image[y: y + h, x: x + w])
+        nbr = os.path.split(image_path)[1].split(".")[0]
+        if nbr == "olivia":
+            nbr = 1
+        else:
+            nbr = 2
+        if nbr == nbr_predicted:
+            print "{} is Correctly Recognized with confidence {}".format(nbr, conf)
+        else:
+            print "{} is Incorrectly Recognized as {} confidence {}".format(nbr, nbr_predicted, conf)
+        cv2.waitKey(1000)
